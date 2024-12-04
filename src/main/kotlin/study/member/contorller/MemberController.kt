31f4cd46e9e42d0802e71146bc0863916016e09c
sycuuui <102959791+sycuuui@users.noreply.github.com>
2024-12-04 21:04:54 +0900

    #1 feat: member 생성 api 구현

--- src/main/kotlin/study/member/contorller/MemberController.kt
+++ src/main/kotlin/study/member/contorller/MemberController.kt
@@ -1,7 +1,17 @@
 package study.member.contorller
 
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import study.member.dto.response.MemberReponse
+import study.member.dto.resquest.MemberRequest
+import study.member.service.MemberService
 
 @RestController
-class MemberController {
+@RequestMapping("/members")
+class MemberController(val memberService: MemberService) {
+    @PostMapping
+    fun save(@RequestBody memberRequest: MemberRequest): MemberReponse =
+            memberService.saveMember(memberRequest)
 }
