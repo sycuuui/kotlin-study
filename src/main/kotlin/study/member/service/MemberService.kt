99a08dd4e680d48d01952944fa1ac4a08f055acd
sycuuui <102959791+sycuuui@users.noreply.github.com>
2024-12-04 21:04:54 +0900

    #1 feat: member 생성 api 구현

--- src/main/kotlin/study/member/service/MemberService.kt
+++ src/main/kotlin/study/member/service/MemberService.kt
@@ -1,7 +1,27 @@
 package study.member.service
 
 import org.springframework.stereotype.Service
+import study.member.dto.response.MemberReponse
+import study.member.dto.resquest.MemberRequest
+import study.member.entity.Member
+import study.member.repository.MemberRepository
 
 @Service
-class MemberService {
+class MemberService(
+        private val memberRepository: MemberRepository
+) {
+    fun saveMember(memberRequest: MemberRequest): MemberReponse {
+        val member = Member(
+                name = memberRequest.name,
+                email = memberRequest.email
+        )
+
+        val savedMember = memberRepository.save(member)
+
+        return MemberReponse(
+                id = savedMember.id,
+                name = savedMember.name,
+                email = savedMember.email
+        )
+    }
 }
