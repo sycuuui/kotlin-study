49906d9513947a4b7ee779425eff5da5271e5491
sycuuui <102959791+sycuuui@users.noreply.github.com>
2024-12-04 20:39:04 +0900

    #1 feat: member관련 로직 클래스 생성

---
+++ src/main/kotlin/study/member/repository/MemberRepository.kt
@@ -0,0 +1,7 @@
+package study.member.repository
+
+import org.springframework.data.jpa.repository.JpaRepository
+import study.member.entity.Member
+
+interface MemberRepository : JpaRepository<Member,Long> {
+}
---
+++ src/main/kotlin/study/member/service/MemberService.kt
@@ -0,0 +1,7 @@
+package study.member.service
+
+import org.springframework.stereotype.Service
+
+@Service
+class MemberService {
+}
